a627864984edd0cecd90c4be657408d002da6831
aramaki-105-j <aramaki.105.j@gmail.com>
2024-07-21 01:11:58 +0900

    settings.pyをDEBUG=False

--- myproject/settings.py
+++ myproject/settings.py
@@ -29,7 +29,7 @@ env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG', default=False)
 
 if DEBUG:   
     ALLOWED_HOSTS = ['*']
